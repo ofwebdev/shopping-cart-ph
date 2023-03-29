dc03629155551ac4d31314d135c7473f31d770e3
ofwebdev <nkomorfaruk@gmail.com>
2023-03-29 16:11:35 +0600

    Add event listner

--- src/component/product/Product.jsx
+++ src/component/product/Product.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import "./product.css";
 
 function Product(props) {
+  // console.log(props);
   const { img, name, seller, ratings, price } = props.product;
+  const handelAddToCard = props.handelAddToCard;
+
   return (
     <div className="product-view">
-      <div>{name}</div>
       <img className="product-img-control" src={img} alt={`product`} />
+
+      <div>{name.substring(0, 18)}...</div>
+
+      <div>
+        <strong>Rating {ratings} Stars</strong>
+      </div>
+      <div>
+        <strong>Price ${price}</strong>
+      </div>
+
+      <button onClick={() => handelAddToCard(props.product)}>
+        Add to card
+      </button>
     </div>
   );
 }
--- src/component/shop/Shop.jsx
+++ src/component/shop/Shop.jsx
@@ -5,20 +5,44 @@ import Product from "../product/Product";
 function Shop() {
   const [products, setProducts] = useState([]);
 
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     fetch("./products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
 
+  // We just provide this function in props
+  // how awesome is it
+  // use should grape this function in your component props
+  const handelAddToCard = (product) => {
+    console.log(product);
+  };
+
   return (
     <div className="container">
       <div className="product-row">
-        {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
-        ))}
+        <div className="product-grid">
+          {products.map((product) => (
+            <Product
+              key={product.id}
+              product={product}
+              handelAddToCard={handelAddToCard}
+            ></Product>
+          ))}
+        </div>
+        <div className="card-details">
+          <h4>Order history</h4>
+          <p>Selected Item : </p>
+          <p>Total Price : $</p>
+          <p>Total shipping charge: $</p>
+          <p>Tax : </p>
+          <h3>Grand Total : </h3>
 
-        <div className="card-details"></div>
+          <button>Clear card</button>
+          <button>Review Order</button>
+        </div>
       </div>
     </div>
   );
